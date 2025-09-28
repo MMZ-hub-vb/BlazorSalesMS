47dce84f695c4898ec3a14c95a3bb82cb3860eff
MirzaIslam <mhafuz_123@hotmail.com>
2025-09-28 15:55:18 +0600

    28/9

---
+++ BlazorSalesMS/Data/UilityFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSalesMS.Data
+{
+    public class UilityFunction
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _dbFactory;
+
+        public UilityFunction(IDbContextFactory<ApplicationDbContext> dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+        public async Task<bool> CheckDuplicateAsync<TEntity>(
+         Func<ApplicationDbContext, DbSet<TEntity>> dbSetSelector,
+         Expression<Func<TEntity, bool>> predicate) where TEntity : class
+        {
+            using var context = _dbFactory.CreateDbContext();
+
+            var exists = await dbSetSelector(context).AnyAsync(predicate);
+            return !exists; // true = no duplicate, false = duplicate found
+        }
+    }
+}
